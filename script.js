bd5ba823f4c74bb21dbf1104599add6c9e8754b4
Joshua Brinson <joshbrin2000@gmail.com>
2023-06-18 18:29:21 -0230

    added deletion of books with delete button

--- script.js
+++ script.js
@@ -28,15 +28,27 @@ function printLibrary(){
     for (i = 0; i < myLibrary.length; i++){
         let card = document.createElement('div');
         card.className = 'book-card';
+        card.id = 'book-card' + i;
+        
         let titP = document.createElement('p');
         let authP = document.createElement('p');
         let pagesP = document.createElement('p');
+        let closeBtn = document.createElement('button');
+
+        closeBtn.innerHTML = "Delete";
+        closeBtn.addEventListener ("click", function() {
+            document.getElementsByClassName("main")[0].removeChild(document.getElementById(card.id));
+          });
+
         titP.textContent = `Title: ${myLibrary[i].title}`;
         authP.textContent += `Author: ${myLibrary[i].author}`;
         pagesP.textContent += `Number of Pages: ${myLibrary[i].pages}`;
+
         card.appendChild(titP);
         card.appendChild(authP);
         card.appendChild(pagesP);
+        card.appendChild(closeBtn);
+        
         document.getElementsByClassName("main")[0].appendChild(card);
     }
 }
